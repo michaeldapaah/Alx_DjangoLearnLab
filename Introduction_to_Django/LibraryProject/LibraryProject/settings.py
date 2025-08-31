2f7fad535b1932e77519d48e2b51630eaaa123e6
MSEC <msec@localhost.localdomain>
2025-08-31 08:39:56 +0000

    Implementing and Interacting with Django Models

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0pw=8(cbv%68_moqcd+ap2g(t)d_hla3zt2wtx9fsg8t(oo1%6'
+SECRET_KEY = 'django-insecure-rol0dv_908bh%c=n9_0p_hht5+#r(b-l11p!a(bpv!5j@c(uxg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
