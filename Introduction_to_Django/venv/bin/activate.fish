dd4b49c6a8e0713c2d9365cfb383a31e3ddcd95b
MSEC <msec@localhost.localdomain>
2025-09-14 01:11:51 +0000

    django modeling in django :

--- Introduction_to_Django/venv/bin/activate.fish
+++ Introduction_to_Django/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/msec/Desktop/Alx_DjangoLearnLab/venv
+set -gx VIRTUAL_ENV /home/msec/Desktop/Alx_DjangoLearnLab/Introduction_to_Django/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
