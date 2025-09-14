014f2741a3149d1c03be0dad5041425744e4b43e
MSEC <msec@localhost.localdomain>
2025-09-14 08:32:47 +0000

    Implementing user authentication in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView
+from .views import list_books, LibraryDetailView, register, LoginView, LogoutView
+from . import views
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),   # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
-    path("register/", register, name="register"),
+    # Register view
+    path("register/", views.register, name="register"),
+
+    # Login view
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+
+    # Logout view
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),
 ]
