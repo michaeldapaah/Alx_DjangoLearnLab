587692added91d4412e559a8de15246bb79c0abc
MSEC <msec@localhost.localdomain>
2025-09-14 07:46:06 +0000

    django views and url configuration

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -11,6 +11,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    publication_year = models.IntegerField(null=True, blank=True) 
 
     def __str__(self):
         return self.title
