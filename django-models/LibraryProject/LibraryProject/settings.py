71822e3fd08415baf6cc25fa82d02de89d556ccb
MSEC <msec@localhost.localdomain>
2025-09-14 08:26:41 +0000

    Implementing user authentication in django

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -70,6 +70,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LibraryProject.wsgi.application'
 
+LOGIN_REDIRECT_URL = "list_books"
+LOGOUT_REDIRECT_URL = "login"
+
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),   # function-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("register/", register, name="register"),
 ]
