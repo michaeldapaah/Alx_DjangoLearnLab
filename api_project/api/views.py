7140be61533bd405cae796f6d740cc4fa2cca039
MSEC <msec@localhost.localdomain>
2025-09-21 00:07:56 +0000

    implementing CRUD operations with Viewset and router in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 # Create your views here.
 
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """A viewset that provides CRUD operations for the Book model."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
